873ce2d070dbeb6cc38f9609b54eab6075aece8f
Zenx007 <zenxpokas@gmail.com>
2025-03-02 18:27:48 -0300

    passwordgenerator

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+if __name__ == "__main__":
+    length = int(input("Enter password length: "))
+    password = generate_password(length)
+    print(f"Your generated password is: {password}")
